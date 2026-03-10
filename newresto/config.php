b1ce663363502f8780640e7d567a231e8e3f90db
krbyjnnn <k.palconete.559180@umindanao.edu.ph>
2026-03-11 07:23:36 +0800

    second commit

--- newresto/config.php
+++ newresto/config.php
@@ -1,13 +1,13 @@
 <?php
-$host = "localhost";
+$hostname = "localhost";
 $dbname = "newresto";
 $username = "root";
 $password = "";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+} catch(PDOException $e) {
+    die("Connection failed". $e->getMessage());
 }
 ?>
