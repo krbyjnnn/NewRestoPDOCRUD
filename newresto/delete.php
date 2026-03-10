c2c3759c5b858ff389d6be757c69c24ddc5f8d06
krbyjnnn <k.palconete.559180@umindanao.edu.ph>
2026-03-11 07:24:38 +0800

    second commit

--- newresto/delete.php
+++ newresto/delete.php
@@ -2,13 +2,9 @@
 require 'config.php';
 
 if (isset($_GET['delete'])) {
-    $c_id = $_GET['delete'];
-
-    $stmt1 = $pdo->prepare("DELETE FROM orders WHERE customer_id = ?");
-    $stmt1->execute(['c_id']);
-
-    $stmt2 = $pdo->prepare("DELETE FROM customers WHERE customer_id = ?");
-    $stmt2->execute(['c_id']);
+    $order_id = $_GET['delete'];
+    $stmt = $pdo->prepare("DELETE FROM orders WHERE order_id = ?");
+    $stmt->execute([$order_id]);
 
     header("Location: landing.php");
     exit();
